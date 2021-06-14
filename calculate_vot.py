297b1487f1cc904c70431d1d4e96981910aeea20
Ernesto Gutiérrez <ernesto@dhcp-168-105-82-170.wireless.manoa.hawaii.edu>
2021-06-14 10:21:00 -1000

    updated function and added the autovot plug-in files

--- calculate_vot.py
+++ calculate_vot.py
@@ -1,19 +1,34 @@
 import parselmouth
 from parselmouth.praat import call as pcall
+from praatio import tgio
 
 def calculateVOT(wav, TextGrid):
 
 	psnd = parselmouth.Sound(wav)
 
-	tg = parselmouth.TextGrid
+	# tg = parselmouth.TextGrid  #this assigns the class; it does not turn it into a praat data
+	# tg = pcall(psnd, "To TextGrid", "vot", "")  #this creates an empty textgrid
+	tg = tgio.openTextgrid(TextGrid)  #this creates an praatio.tgio.Textgrid object
+	print(tg)
 
-	new_tg = pcall([psnd, tg], "AutoVOT", 
+	## The code below applies to functions from Praat plugins
+	parselmouth.praat.run_file([psnd,tg], 
+		"autovot-0.94/autovot/praat_plugin/AutoVOT_Praat_plugin_v0.94/plugin_autovot/autovot.praat", 
 		"stops", 
 		"*", 
 		"mono", 
 		5, 
 		500, 
 		"models/vot_predictor.amanda.max_num_instances_1000.model")
+
+	## The code below only applies to Praat's native functions
+	# new_tg = pcall([psnd, tg], "AutoVOT", 
+	# 	"stops", 
+	# 	"*", 
+	# 	"mono", 
+	# 	5, 
+	# 	500, 
+	# 	"models/vot_predictor.amanda.max_num_instances_1000.model")
 
 	return duration
 
