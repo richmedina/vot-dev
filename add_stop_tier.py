b3f9d596562aa6f30cc6cd3085b4cf6c66d1ea0d
Ernesto Gutiérrez <ernesto@dhcp-168-105-82-170.wireless.manoa.hawaii.edu>
2021-06-18 10:37:34 -1000

    modified func

--- add_stop_tier.py
+++ add_stop_tier.py
@@ -3,8 +3,6 @@ from praatio import tgio
 
 def addStopTier(TextGrid, stops=[], startPadding=0, endPadding=0):
 
-	fileCheck(TextGrid) #??
-
 	# open textgrid and turn the tier labels to lowercase strings
 	tg = tgio.openTextgrid(TextGrid)
 	tg.tierNameList = [tier.lower() for tier in tg.tierNameList]
@@ -13,63 +11,69 @@ def addStopTier(TextGrid, stops=[], startPadding=0, endPadding=0):
 	# collect all word tiers and terminate process if none exists
 	allWordTiers = [tierName for tierName in tg.tierNameList if 'word' in tierName]
 	if len(allWordTiers) == 0:
-		print("This TextGrid file does not contain a tier named 'Words'.\n")
-		return
+		print(TextGrid,"does not contain any tier named 'Words'.\n")
+		return False #return False
 
 	# collect all phone tiers and terminate process if none exists
 	allPhoneTiers = [tierName for tierName in tg.tierNameList if 'phone' in tierName]
 	if len(allPhoneTiers) == 0:
-		print("This TextGrid file does not contain a tier named 'Phones'.\n")
-		return
+		print(TextGrid,"does not contain any tier named 'Phones'.\n")
+		return False #return False
 
-	# verify that an equal number of word and phone tiers exists to continue
+	# verify that an equal number of word and phone tiers exists before continuing
 	if len(allWordTiers) == len(allPhoneTiers):
 
 		voicedTokens =[]
 		totalSpeakers = len(allPhoneTiers)
 		currentSpeaker = 0
-		voicedWarning =	False
+		lastSpeaker =	False
+		populatedTiers = 0
 
-		for tier in allPhoneTiers:
+		for tierName in allPhoneTiers:
 			currentSpeaker += 1
-			if 'phone' in tier and tier.replace('phone', 'word') in allWordTiers:
-				speakerName = tier.split("phone")[0]
-				phoneTier = tg.tierDict[tier]
-				wordTier = tg.tierDict[tier.replace('phone','word')]
+			if tierName.replace('phone', 'word') in allWordTiers:
+				speakerName = tierName.split("phone")[0]
+				phoneTier = tg.tierDict[tierName]
+				wordTier = tg.tierDict[tierName.replace('phone','word')]
 				wordStartTimes = [entry[0] for entry in wordTier.entryList]
 				if totalSpeakers == currentSpeaker:
-					voicedWarning = True
-				tg.addTier(
-					processStopTier(
+					lastSpeaker = True
+				newTier = processStopTier(
 						TextGrid, 
 						speakerName, 
 						phoneTier, 
 						wordStartTimes, 
 						stops, 
+						populatedTiers, 
 						startPadding, 
 						endPadding, 
 						voicedTokens, 
-						voicedWarning
+						lastSpeaker
 						)
-					)
+				if newTier:
+					populatedTiers += 1
+					tg.addTier(newTier)
+				else:
+					continue
 			
 			else:
-				print("The names of the 'word' and 'phone' tiers are inconsistent in file",TextGrid+".","Fix the issue before continuing.\n")
-				return
-				#stop
+				print("The names of the 'word' and 'phone' tiers are inconsistent in file",\
+					TextGrid+".","Fix the issue before continuing.\n")
+				return False #return False
+	
 	else:
-		print("Error: There isn't an even number of 'phone' and 'word' tiers per speaker in file",TextGrid,". Fix the issue before continuing.\n")
-		return
-		#stop
+		print("Error: There isn't an even number of 'phone' and 'word' tiers per speaker in file",\
+			TextGrid,". Fix the issue before continuing.\n")
+		return False #return False
 
 	# save the new textgrid with a 'stop' tier
 	saveName = TextGrid.split(".TextGrid")[0]
-	tg.save(saveName+"_output.TextGrid")
+	tg.save(saveName+"_output.TextGrid") #add output directory to store temp tg?
 	
 	return
 
 
-def processStopTier(TextGrid, speakerName, phoneTier, wordStartTimes, stops, startPadding, endPadding, voicedTokens, voicedWarning): 
+def processStopTier(TextGrid, speakerName, phoneTier, wordStartTimes, stops, populatedTiers, startPadding, endPadding, voicedTokens, lastSpeaker): 
 
 	# specify stop categories
 	ipaStops = ['p', 'b', 't', 'd', 'ʈ', 'ɖ', 'c', 'ɟ', 'k', 'g', 'q', 'ɢ', 'ʔ', "p'", "t'", "k'", 'ɓ', 'ɗ', 'ʄ', 'ɠ', 'ʛ']
@@ -88,7 +92,7 @@ def processStopTier(TextGrid, speakerName, phoneTier, wordStartTimes, stops, sta
 				print("'"+stopSymbol+"'","is not a stop sound. This symbol will be ignored.\n")
 		stops = tempStops
 
-	# identify stops of interest
+	# identify stops of interest in the TextGrid
 	stopEntryList = []
 	for entry in phoneTier.entryList:
 		if entry[-1].lower() in stops and entry[0] in wordStartTimes:
@@ -102,53 +106,50 @@ def processStopTier(TextGrid, speakerName, phoneTier, wordStartTimes, stops, sta
 		extendedEntryList.append([start+startPadding, stop+endPadding, label])
 
 	# check for length requirements and resolve overlapping conflicts
-	fileName = TextGrid
 	for interval in range(len(extendedEntryList)-1):
+		#intA = current interval
+		#intB = next interval
 		if extendedEntryList[interval][1]-extendedEntryList[interval][0] < 0.025:
 			extendedEntryList[interval][1] = extendedEntryList[interval][0] + 0.025
 			if extendedEntryList[interval+1][0] < extendedEntryList[interval][1]:
 				if extendedEntryList[interval+1][0] - extendedEntryList[interval][1] > 0.075:
 					print("Error: there is a significant overlap between two phone segments at time:",\
-						extendedEntryList[interval][1],"in file:",fileName,". Fix the issue before continuing.\n")
+						extendedEntryList[interval][1],"in file:",TextGrid,". Fix the issue before continuing.\n")
 					#stop function -- too big of a shift
-				print("Error: There are two segments in file:",fileName,"around time:",extendedEntryList[interval][1],\
-					"that either overlap with each other, or are too short to calculate VOT. Fix the issue before continuing.")
-				print("This could be an issue resulting from:"\
-					"\n(1) very fast speech, \n(2) two tokens very close to each other, or \n(3) too much 'padding'.\n")
-				#stop function -- too much of an overlap
+				# 	return # sys.exit()
+				# print("Error: There are two segments in file:",TextGrid,"around time:",extendedEntryList[interval][1],\
+				# 	"that either overlap with each other, or are too short to calculate VOT. Fix the issue before continuing.")
+				# print("This could be an issue resulting from:"\
+				# 	"\n(1) very fast speech, \n(2) two tokens very close to each other, or \n(3) too much 'padding'.\n")
+				# #stop function -- too much of an overlap
 				return
 			else:
-				print("Note: the segment in file:",fileName,"at time:",extendedEntryList[interval][0],\
+				print("Note: the segment in file:",TextGrid,"at time:",extendedEntryList[interval][0],\
 				"was automatically elongated because it was shorter than the required 25 ms.\n")
 		if extendedEntryList[interval+1][0] < extendedEntryList[interval][1]:
 			extendedEntryList[interval+1][0] = extendedEntryList[interval][1]
-			print("Note: the start time for the segment in file:",fileName,"at time:",extendedEntryList[interval+1][0],\
+			print("Note: the start time for the segment in file:",TextGrid,"at time:",extendedEntryList[interval+1][0],\
 				"was shifted back to resolve an issue with overlapping segments.")
 			print("The issue with overlapping times could be an issue resulting from:",\
 				"\n(1) very fast speech, \n(2) two tokens very close to each other, or \n(3) too much 'padding'.\n")
 
 	# provide warning for voiced tokens
-	if len(voicedTokens) > 0 and voicedWarning:
-		print("***Warning: You're trying to obtain VOT calculations of the following voiced stops: ",list(set(voicedTokens)),".")
+	if len(voicedTokens) > 0 and lastSpeaker:
+		print("***Warning: You're trying to obtain VOT calculations of the following voiced stops: ",list(set(voicedTokens)))
 		print("Note that AutoVOT's current model only works on voiceless stops; "\
 			"prevoicing in the productions may result in inaccurate calculations.\n")
 
-	# construct the stop tier
-	stopTier = phoneTier.new(name = speakerName+"stops", entryList = extendedEntryList)
-
-	return stopTier
-
-def fileCheck(TextGrid):
-	name, ext = os.path.splitext(TextGrid)
-	if ext != '.TextGrid':
-		print(TextGrid,"is not a .TextGrid file.\n")
-		#add stop to the entire program
+	# construct the stop tier if stops were identified
+	if len(extendedEntryList) > 0:
+		stopTier = phoneTier.new(name = speakerName+"stops", entryList = extendedEntryList)
+		return stopTier
+	elif lastSpeaker and populatedTiers == 0:
+		print("There were no voiceless stops found in",TextGrid+".\n")
+		return False
 	else:
-		print("All is good with the format of file",TextGrid,"\n")
 		return
 
-
-addStopTier('test1.TextGrid',['k','b','g'])
+addStopTier('test1.1.TextGrid')
 
 
 
--- calculate_vot.py
+++ calculate_vot.py
@@ -1,15 +1,26 @@
 import parselmouth
-import tgt
+import os
+# import tgt
 
 def calculateVOT(wav, TextGrid):
 
+	# verify file format
+	if not fileCheck(wav, TextGrid):
+		return
+
 	# process the data
 	psnd = parselmouth.Sound(wav)
-	ptg = parselmouth.TextGrid.from_tgt(tgt.read_textgrid(TextGrid))
+	# ptg = parselmouth.TextGrid.from_tgt(tgt.read_textgrid(TextGrid))
+	ptg = parselmouth.praat.call("Read from file", TextGrid)
+	# print(type(ptg))
+	# print(ptg)
 
 	# apply AutoVOT
 	obj = parselmouth.praat.run_file([psnd,ptg],
-		# "Users/ernesto/Library/Preferences/Praat\ Prefs/plugin_autovot/autovot.praat", # file on local machine # how to use this line instead of the following line?
+		# "/Users/ernesto/Library/Preferences/Praat\ Prefs/plugin_autovot/autovot.praat", 
+		# file on local machine # how to use this line instead of the following line?
+		# add initial slash
+		# try one dot ./
 		"autovot-0.94/autovot/praat_plugin/AutoVOT_Praat_plugin_v0.94/plugin_autovot/autovot.praat", # file on repository
 		# "utt - stops", 
 		# "*", 
@@ -22,4 +33,34 @@ def calculateVOT(wav, TextGrid):
 
 	return
 
-calculateVOT('test_audio.wav', "test1_output.TextGrid")
+def fileCheck(wav, TextGrid):
+
+	wavName, wavExt = os.path.splitext(wav)
+	textgridName, textgridExt = os.path.splitext(TextGrid)
+
+	if wavExt != '.wav' and textgridExt != '.TextGrid':
+		print("The files",wav,"and",TextGrid,"do not meet format requirements;",\
+			wav,"must be a wav file, and",TextGrid,"must be a TextGrid file.\n")
+		return False
+
+	else:
+		print("All is good with the format of files",wav,"and",TextGrid+".\n")
+		return True
+		#sys.exit() 0=no issue, 1=exit with error
+
+
+
+calculateVOT('test.wav', "test_output.TextGrid")
+
+
+
+
+
+
+
+
+
+
+
+
+
